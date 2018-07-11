39c7379fae9b923753936ebe47be2b7703eaa0f6
Ryanight <ryanighty@gmail.com>
2018-07-11 21:24:53 +0800

    Create index.py

---
+++ Web/index.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+from flask import jsonify
+
+app = Flask(__name__, )
+app.config['SECRET_KEY'] = "dfdfdffdad"
+
+
+@app.route('/')
+def login():
+    return render_template('untitled.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/mystring')
+def mystring():
+    return 'my string'
+
+
+@app.route('/dataFromAjax')
+def dataFromAjax():
+    test = request.args.get('mydata')
+    print(test)
+    return 'dataFromAjax'
+
+
+@app.route('/mydict', methods=['GET', 'POST'])
+def mydict():
+    d = {'name': 'xmr', 'age': 18}
+    return jsonify(d)
+
+
+@app.route('/mylist')
+def mylist():
+    l = ['xmr', 18]
+    return jsonify(l)
+
+
+@app.route('/js_call')
+def js_call():
+    username = request.args.get('email')
+    password = request.args.get('password')
+    print(username)
+    print(password)
+    # to send the command by ssh : os.system("ssh user@host \' restart(command) \' ")
+    if username == "123" and password == "test":
+        return "loginsuccess"
+    else:
+        return "login failed"
+
+
+if __name__ == '__main__':
+    app.run()
